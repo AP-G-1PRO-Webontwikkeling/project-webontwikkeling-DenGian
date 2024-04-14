352dd64bbb02cf7ca4889c33399190cf97c4328c
DenGian <mondelaers.ian@gmail.com>
2024-04-14 13:28:29 +0000

    initial commit

---
+++ lib/cjs/interfaces/programming-language.interface.d.ts
@@ -0,0 +1,19 @@
+export interface ProgrammingLanguage {
+    id: string;
+    name: string;
+    description: string;
+    age: number;
+    isActive: boolean;
+    birthDate: string;
+    imageUrl: string;
+    genre: string;
+    useCases: string[];
+    relatedLibrary: {
+        id: string;
+        name: string;
+        description: string;
+        website: string;
+        stars: number;
+        latestVersion: string;
+    };
+}
---
+++ lib/cjs/utils/helper-functions.d.ts
@@ -0,0 +1,4 @@
+declare function getAllLang(): Promise<void>;
+declare function getAllLib(): Promise<void>;
+declare function filterById(id: string): Promise<void>;
+export { getAllLang, getAllLib, filterById };
