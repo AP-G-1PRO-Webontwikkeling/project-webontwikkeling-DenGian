d343127661ab8f146c89fcbc8a40e247e59c2afe
DenGian <mondelaers.ian@gmail.com>
2024-05-01 15:27:23 +0000

    divided routes in a more specific way

---
+++ src/routes/contact.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.type("text/html");
+    res.render("contact");
+});
+
+export default router;
