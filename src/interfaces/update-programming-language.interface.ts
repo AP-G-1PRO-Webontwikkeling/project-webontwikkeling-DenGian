d05f257be595f129a33235c3a3a5fe072c64d8b6
DenGian <mondelaers.ian@gmail.com>
2024-05-08 21:44:29 +0000

    add interface for updating a language

---
+++ src/interfaces/update-programming-language.interface.ts
@@ -0,0 +1,8 @@
+export interface UpdateProgrammingLanguage {
+    name: string;
+    birthdate: string;
+    genre: string;
+    isActive: boolean;
+    description: string;
+    useCases: string[];
+}
