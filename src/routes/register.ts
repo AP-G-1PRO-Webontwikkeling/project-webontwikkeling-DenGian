0c6416ab1da6a5d74295c7aae62bb02dcc27a711
DenGian <mondelaers.ian@gmail.com>
2024-05-10 21:42:29 +0000

    add register route

---
+++ src/routes/register.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import { registerUser, isEmailRegistered, isUsernameRegistered } from "../config/database";
+import { validateFormData } from "../middleware/handleFormValidation";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+    res.type("text/html");
+    res.render("register");
+});
+
+router.post("/", validateFormData, async (req: Request, res: Response) => {
+    try {
+        const { email, password, username } = req.body;
+        if (!email || !password || !username) {
+            return res.status(400).render("niceTry");
+        }
+        const emailExists = await isEmailRegistered(email);
+        if (emailExists) {
+            req.session.message = { type: "error", message: "Email is already registered." };
+            return res.redirect("/register");
+        }
+        const usernameExists = await isUsernameRegistered(username);
+        if (usernameExists) {
+            req.session.message = { type: "error", message: "Username is already taken." };
+            return res.redirect("/register");
+        }
+        await registerUser(email, password, username);
+        req.session.message = { type: "success", message: "Registration successful" };
+        res.redirect("/login");
+    } catch (e: any) {
+        req.session.message = { type: "error", message: e.message };
+        console.error("Error registering user:", e);
+        res.status(500).redirect("/register");
+    }
+});
+
+export default router;
